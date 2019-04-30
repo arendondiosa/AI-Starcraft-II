90e66355eb628ba41f0935a70b198063d40a727c
Alejandro E. Rendon <alejandro@rendon.co>
2019-04-29 22:23:50 -0500

    Add more supply space

--- intro.py
+++ intro.py
@@ -1,14 +1,28 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import NEXUS, PROBE, PYLON
 
 
-class Bot(sc2.BotAI):
+class BotTest(sc2.BotAI):
     async def on_step(self, iteration):
         await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
 
+    async def build_workers(self):
+        for nexus in self.units(NEXUS).ready.noqueue:
+            if self.can_afford(PROBE):
+                await self.do(nexus.train(PROBE))
+
+    async def build_pylons(self):
+        if self.supply_left < 5 and not self.already_pending(PYLON):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exists:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexuses.first)
 
 run_game(maps.get("AutomatonLE"), [
-    Bot(Race.Zerg, Bot()),
+    Bot(Race.Protoss, BotTest()),
     Computer(Race.Terran, Difficulty.Easy)
 ], realtime=True)
